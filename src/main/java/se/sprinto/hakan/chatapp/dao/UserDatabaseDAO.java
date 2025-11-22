a4229234372149e89956dea54b9b13d91165206d
gmazreku <guven.mazreku@gmail.com>
2025-11-22 23:17:02 +0100

    Nu körs login metoden med LEFT JOIN

--- src/main/java/se/sprinto/hakan/chatapp/dao/UserDatabaseDAO.java
+++ src/main/java/se/sprinto/hakan/chatapp/dao/UserDatabaseDAO.java
@@ -1,5 +1,6 @@
 package se.sprinto.hakan.chatapp.dao;
 
+import se.sprinto.hakan.chatapp.model.Message;
 import se.sprinto.hakan.chatapp.model.User;
 
 import java.sql.*;
@@ -7,28 +8,45 @@ import java.sql.*;
 public class UserDatabaseDAO implements UserDAO{
     @Override
     public User login(String username, String password) {
-        String sql = "SELECT * FROM users WHERE username=? AND password=?";
+        String sql = "SELECT u.id, u.username, u.password, m.id AS message_id, m.message, m.timestamp, m.user_id " +
+                     "FROM users u " +
+                     "LEFT JOIN messages m ON u.id = m.user_id " +
+                     "WHERE u.username = ?";
+
+        User user = null;
 
         try (Connection conn = DatabaseUtil.getInstance().getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
+
             stmt.setString(1, username);
             stmt.setString(2, password);
 
             try (ResultSet rs = stmt.executeQuery()) {
-                if (rs.next()) {
-                    // Kollar om användaren hittas, skapar User objekt.
-                    User user = new User(
-                            rs.getString("username"),
-                            rs.getString("password")
-                    );
-                    user.setId(rs.getInt("id"));
-                    return user;
+                while (rs.next()) {
+                    if (user == null) {
+                        user = new User(
+                                rs.getString("username"),
+                                rs.getString("password")
+                        );
+                        user.setId(rs.getInt("id"));
+                    }
+
+                    if (rs.getString("message") != null) {
+                        Message message = new Message(
+                                rs.getInt("user_id"),
+                                rs.getString("message"),
+                                rs.getTimestamp("timestamp").toLocalDateTime()
+                        );
+                        user.addMessage(message);
+                    }
                 }
             }
+
         } catch (SQLException e) {
-        e.printStackTrace();
-    }
-        return null;
+            e.printStackTrace();
+        }
+
+        return user;
     }
 
     @Override
